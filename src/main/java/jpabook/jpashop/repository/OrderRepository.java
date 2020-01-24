1660e1066ca2df538f845337f0719e4146e892dd
devjjinii <cyujin11010@naver.com>
2020-01-24 13:28:04 +0900

    26강까지 완료

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -1,5 +1,7 @@
 package jpabook.jpashop.repository;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 
 import org.springframework.stereotype.Repository;
@@ -21,7 +23,15 @@ public class OrderRepository {
 		return em.find(Order.class, id);
 	}
 	
-//	public List<Order> findAll(OrderSearch orderSearch) {
-//		
-//	}
+	public List<Order> findAll(OrderSearch orderSearch) {
+		// 동적쿼리 처리 ..... 
+		return em.createQuery("select o from Order o join o.member m" +
+						"where o.status = :status" +
+						"and m.name like :name" , Order.class)
+				.setParameter("status", orderSearch.getOrderStatus())
+				.setParameter("name", orderSearch.getMemberName())
+				//.setFristResult(100) 100부터
+				.setMaxResults(1000) // 최대 1000까지
+				.getResultList();
+	}
 }
