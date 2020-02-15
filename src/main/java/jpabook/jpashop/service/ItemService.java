08cff48b5b1bb8e01dc3760e4e37dc0863cd29a8
devjjinii <cyujin11010@naver.com>
2020-02-15 15:22:21 +0900

    1편 완강

--- src/main/java/jpabook/jpashop/service/ItemService.java
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import jpabook.jpashop.domain.item.Book;
 import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
@@ -20,6 +21,25 @@ public class ItemService {
 	public void saveItem(Item item) {
 		itemRepository.save(item);
 	}
+	
+	/*@Transactional   //준영속 엔티티 수정, 1. 변경 감지 기능
+	public void updateItem(Long itemId, Book param) {
+		Item findItem = itemRepository.findOne(itemId);
+		findItem.setPrice(param.getPrice());
+		findItem.setName(param.getName());
+		findItem.setStockQuantity(param.getStockQuantity());
+	}*/
+	
+	@Transactional  // merge를 사용하지 않는다, null.. (실무)
+	// * 엔티티를 변경할때는 항상 변경 감지를 이용 !        // -> UpdateItemDto 사용해도 된다.
+	public void updateItem(Long itemId, String name, int price, int stockQuantity ) {
+		Item findItem = itemRepository.findOne(itemId);
+		findItem.setName(name);
+		findItem.setPrice(price);
+		findItem.setStockQuantity(stockQuantity);
+		//return findItem;
+	}
+	
 	
 	public List<Item> findItem() {
 		return itemRepository.findAll();
