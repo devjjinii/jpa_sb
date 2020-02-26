63c7942d7114b1ad7b7deca2abdaecf144541e9f
devjjinii <cyujin11010@naver.com>
2020-02-26 14:23:02 +0900

    12강까지 완료

--- src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -1,13 +1,20 @@
 package jpabook.jpashop.api;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import jpabook.jpashop.repository.order.simplequery.OrderSimpleQueryDto;
+import jpabook.jpashop.repository.order.simplequery.OrderSimpleQueryRepository;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
 /*
@@ -23,6 +30,7 @@ import lombok.RequiredArgsConstructor;
 public class OrderSimpleApiController {
 
 	private final OrderRepository orderRepository;
+	private final OrderSimpleQueryRepository orderSimpleQueryRepository;
 	
 	// 무한루프 ..... 양방향일 경우 @JsonIgnore 
 	@GetMapping("/api/v1/simple-orders")
@@ -33,5 +41,50 @@ public class OrderSimpleApiController {
 			order.getDelivery().getAddress();  // Lazy 강제 초기화
 		}
 		return all;
+	}
+	
+	@GetMapping("/api/v2/simple-orders")
+	public List<SimpleOrderDto> orderV2() {
+		// order 2개 
+		// N + 1 -> 1 + 회원 N + 배송 N
+		List<Order> orders = orderRepository.findAllByCriteria(new OrderSearch());
+		
+		List<SimpleOrderDto> result = orders.stream()
+				.map(o -> new SimpleOrderDto(o))
+				.collect(Collectors.toList());
+		
+		return result;
+	}
+	
+	@GetMapping("/api/v3/simple-orders")
+	public List<SimpleOrderDto> orderV3() {
+		List<Order> orders = orderRepository.findAllWithMemberDelivery();
+		
+		List<SimpleOrderDto> result = orders.stream()
+				.map(o-> new SimpleOrderDto(o))
+				.collect(Collectors.toList());
+		return result;
+	}
+	
+	@GetMapping("/api/v4/simple-orders")
+	public List<OrderSimpleQueryDto> orderV4() {
+		return orderSimpleQueryRepository.findOrderDtos();
+	}
+	
+	@Data
+	static class SimpleOrderDto {
+		private Long orderId;
+		private String name;
+		private LocalDateTime orderDate;
+		private OrderStatus orderStatus;
+		private Address address;
+		
+		public SimpleOrderDto(Order order) {
+			orderId = order.getId();
+			name = order.getMember().getName();
+			orderDate = order.getOrderDate();
+			orderStatus = order.getStatus();
+			address = order.getDelivery().getAddress();
+		}
 	}
 }
