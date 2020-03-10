45d5bfa6f5a7345795fcf9c9fb62bec67858dcc0
devjjinii <cyujin11010@naver.com>
2020-03-10 11:25:56 +0900

    :octocat: 완강

---
+++ src/main/java/jpabook/jpashop/repository/MemberRepository1.java
@@ -0,0 +1,14 @@
+//package jpabook.jpashop.repository;
+//
+//import java.util.List;
+//
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//import jpabook.jpashop.domain.Member;
+//
+//public interface MemberRepository1 extends JpaRepository<Member, Long>{
+//																// pk
+//	// select m from Member m where m.name = ?
+//	List<Member> findByName(String name);
+//	
+//}
