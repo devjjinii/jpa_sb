067cee8334006c81799a2635d085331129e75d3d
devjjinii <devjjinii@naver.com>
2020-09-02 23:59:52 +0900

    review start

---
+++ src/main/java/jpa/jin/JinApplication.java
@@ -0,0 +1,19 @@
+package jpa.jin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JinApplication {
+
+	public static void main(String[] args) {
+//		Hello hello = new Hello();
+//		hello.setData("jin");
+//		String data = hello.getData();
+//		//soutv
+//		System.out.println("data = " + data);
+
+		SpringApplication.run(JinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jpa/jin/JinApplicationTests.java
@@ -0,0 +1,13 @@
+package jpa.jin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
