af73bab66b091b85e496c9c704f849ba7c1dd06d
devjjinii <devjjinii@naver.com>
2020-09-12 20:40:26 +0900

    delete test & update memberRepo

--- src/main/java/jpa/jin/repository/MemberRepository.java
+++ src/main/java/jpa/jin/repository/MemberRepository.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.List;
 
 @Repository
 public class MemberRepository {
@@ -12,12 +13,27 @@ public class MemberRepository {
     @PersistenceContext
     private EntityManager em;
 
-    public Long save(Member member) {
+    // 저장
+    public void save(Member member) {
         em.persist(member);
-        return member.getId();
     }
 
-    public Member find(Long id) {
+    // 아이디로 조회
+    public Member findOne(Long id) {
         return em.find(Member.class, id);
     }
+
+    // 전체 조회
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
+
+    // 이름 조회
+    public List<Member> findByName(String name) {
+        return em.createQuery("select m from Member m where m.username = :name", Member.class)
+                .setParameter("name",name)
+                .getResultList();
+    }
+
 }
